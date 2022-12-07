c6a511ea2601d270667e49c156a6c9064d457a5e
Ivo_Broekhof <i.broekhof@student.utwente.nl>
2022-12-07 23:34:05 +0100

    Solved Day 7!

--- src/Day07.hs
+++ src/Day07.hs
@@ -8,7 +8,7 @@ module Day07 where
 
     test = "$ cd /\n$ ls\ndir a\n14848514 b.txt\n8504156 c.dat\ndir d\n$ cd a\n$ ls\ndir e\n29116 f\n2557 g\n62596 h.lst\n$ cd e\n$ ls\n584 i\n$ cd ..\n$ cd ..\n$ cd d\n$ ls\n4060174 j\n8033020 d.log\n5626152 d.ext\n7214296 k"
         
-    testCases = [(test, solve1, 48381165)]
+    testCases = [(test, solve1, 95437), (test, solve2, 24933642)]
 
     type Day07 = [Command]
 
@@ -30,7 +30,7 @@ module Day07 where
                 filename = (\n d e -> n++d++e) <$> many letter <*> (option [] (string ".")) <*> many letter
 
     size :: System -> Integer
-    size (File s _) = s
+    size (File s _) =  s
     size (Dir fs _) = (map size .> sum) fs
 
     name :: System -> String
@@ -41,16 +41,25 @@ module Day07 where
     insert fs [] (Dir _ s) = Dir fs s
     insert fs (p:ps) (Dir sub s) = Dir (modifyWhere (\x -> name x == p) (insert fs ps) sub) s
 
+    findDirs :: System -> (System -> Bool) -> [System]
+    findDirs (File _ _) _ = []
+    findDirs (Dir fs _) p = (filter (\f -> isDir f && p f) fs) ++ (concat $ map (flip findDirs p) fs)
+        where   isDir (File _ _) = False
+                isDir (Dir _ _) = True
+
     solve1 :: Day07 -> Integer
-    solve1 = foldl cmd (Dir [] "/", []) .> fst .> size
-            -- cmd :: (System, [String]) -> Command -> (System, [String])
+    solve1 = foldl cmd (Dir [] "/", []) .> fst .> (flip findDirs (\d -> size d <= 100000)) .> map size .> sum
         where   cmd (sys, _) (CD "/") = (sys, [])
                 cmd (sys, ps) (CD "..") = (sys, init ps)
                 cmd (sys, ps) (CD p) = (sys, ps ++ [p])
                 cmd (sys, ps) (LS fs) = (insert fs ps sys, ps)
     
     solve2 :: Day07 -> Integer
-    solve2 xs = undefined
+    solve2 = foldl cmd (Dir [] "/", []) .> fst .> (\s -> ((flip findDirs (const True)) .> map size .> filter (>= 30000000 - (70000000 - size s))) s) .> minimum
+        where   cmd (sys, _) (CD "/") = (sys, [])
+                cmd (sys, ps) (CD "..") = (sys, init ps)
+                cmd (sys, ps) (CD p) = (sys, ps ++ [p])
+                cmd (sys, ps) (LS fs) = (insert fs ps sys, ps)
 
     day07 = Day testCases puzzle solve1 solve2
 
